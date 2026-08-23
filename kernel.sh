84a2642ad6f5d9a788e9a3a171a8fac44403d521
blebleble save ur day <108147789+xxblebleblexx@users.noreply.github.com>
2026-08-24 06:25:32 +0700

    Change kernel branch from mb-qpr2 to stable

--- kernel.sh
+++ kernel.sh
@@ -1,6 +1,6 @@
 #configuration
 kernel_source=https://github.com/xxblebleblexx/android_kernel_xiaomi_gale.git
-branch_kernel=mb-qpr2
+branch_kernel=stable
 defconfig_path=arch/arm64/configs/gale_defconfig
 defconfig=gale_defconfig
 
