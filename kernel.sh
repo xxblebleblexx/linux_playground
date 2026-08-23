11ba065e19110c710e602c2537f743bd6c34b7a9
blebleble save ur day <108147789+xxblebleblexx@users.noreply.github.com>
2026-08-23 15:35:48 +0700

    Change kernel source and branch in kernel.sh
    Updated kernel source and branch for compilation.

--- kernel.sh
+++ kernel.sh
@@ -1,6 +1,6 @@
 #configuration
-kernel_source=https://github.com/aosp-mirror/kernel_common.git
-branch_kernel=deprecated/android15-6.6-2024-08
+kernel_source=https://github.com/xxblebleblexx/android_kernel_xiaomi_gale_6.6.git
+branch_kernel=dev
 defconfig_path=arch/arm64/configs/gki_defconfig
 defconfig=gki_defconfig
 
@@ -18,4 +18,4 @@ curl -LSs "https://raw.githubusercontent.com/ReSukiSU/ReSukiSU/main/kernel/setup
 #KSU config
 echo "CONFIG_KSU=y" >> $defconfig_path
 #Run compile
-make O=out ARCH=arm64 $defconfig; printf "n\n2\n\n\n\nY\n" | make -j$(nproc --all) KCFLAGS="-Wno-error=-Wmacro-redefined" CC=clang O=out ARCH=arm64 LLVM=1 LLVM_IAS=1 LD=ld.lld AS=llvm-as AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump READELF=llvm-readelf STRIP=llvm-strip
+make O=out ARCH=arm64 $defconfig; printf "n\n2\n\n\n\nY\n" | make -j$(nproc --all) KCFLAGS="-Wno-error=-macro-redefined" CC=clang O=out ARCH=arm64 LLVM=1 LLVM_IAS=1 LD=ld.lld AS=llvm-as AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump READELF=llvm-readelf STRIP=llvm-strip
