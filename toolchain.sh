5987e0bd6046f646aff7532a88e6e560fd9971e3
blebleble save ur day <108147789+xxblebleblexx@users.noreply.github.com>
2026-09-13 16:31:13 +0700

    Modify toolchain.sh for package and git setup
    Updated package installation and added git configuration.

--- toolchain.sh
+++ toolchain.sh
@@ -1,19 +1,7 @@
 #!/bin/sh
 
 #setup mandatory packages
-sudo pacman -Sy --noconfirm git patch llvm lld base-devel ncurses openssl bc flex bison rsync kmod cpio python ccache tar gzip zstd wget;wait
-#Toolchain compiler
-wget https://github.com/Madara273/Sakura-Clang-Compiler/releases/download/Sakura-Clang-R01/sakura-clang-24.0.0.tar.zst;wait
-mkdir -p clang
-mv sakura-clang-24.0.0.tar.zst clang/
-cd clang
-tar --zstd -xf sakura-clang-24.0.0.tar.zst
-
-#MLGO
-wget https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/refs/heads/main/mlgo-models.tar.gz
-tar -zxvf mlgo-models.tar.gz
-cd ..
-
-#test clang
-export PATH=$(pwd)/clang/bin:$PATH
-clang --version
+sudo pacman -Sy --noconfirm git repo patch llvm lld base-devel ncurses openssl bc flex bison rsync kmod cpio python ccache tar gzip zstd wget;wait
+#repo dummy email
+git config user.email "fufufafa@gmail.com"
+git config user.name "fufufafa"
