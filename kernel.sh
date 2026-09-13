725e2840ad0da8713d16f9254b76d50446a7e010
blebleble save ur day <108147789+xxblebleblexx@users.noreply.github.com>
2026-09-13 16:38:00 +0700

    Add patch to disable check_defconfig
    Added a patch from a GitHub commit to disable check_defconfig.

--- kernel.sh
+++ kernel.sh
@@ -7,6 +7,9 @@ repo init -u $kernel_source -b $branch_kernel
 repo sync -c -j$(nproc) --no-tags
 cd common
 
+#disable check_defconfig
+wget https://github.com/xxblebleblexx/android_kernel_xiaomi_gale_6.6/commit/f4814d6dfd114d08b834117129bec247d89767c1.diff
+patch -p1 < f4814d6dfd114d08b834117129bec247d89767c1.diff
 #Resukisu
 curl -LSs "https://raw.githubusercontent.com/ReSukiSU/ReSukiSU/main/kernel/setup.sh" | bash
 
