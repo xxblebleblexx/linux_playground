91cd248c10d2049982db9b10b4e76009b8932168
blebleble save ur day <108147789+xxblebleblexx@users.noreply.github.com>
2026-09-13 16:27:45 +0700

    Add repo sync command in kernel.sh
    Added repo sync command to synchronize kernel sources.

--- kernel.sh
+++ kernel.sh
@@ -4,6 +4,7 @@ branch_kernel=common-android15-6.6
 defconfig_path=common/arch/arm64/configs/gki_defconfig
 #Kernel clone
 repo init -u $kernel_source -b $branch_kernel
+repo sync -c -j$(nproc) --no-tags
 cd common
 
 #Resukisu
