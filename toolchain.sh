8c475e9230170eb38310681c385fde4918c0b78c
blebleble save ur day <108147789+xxblebleblexx@users.noreply.github.com>
2026-09-13 16:42:30 +0700

    Update git config to use global settings

--- toolchain.sh
+++ toolchain.sh
@@ -3,5 +3,5 @@
 #setup mandatory packages
 sudo pacman -Sy --noconfirm git repo patch llvm lld base-devel ncurses openssl bc flex bison rsync kmod cpio python ccache tar gzip zstd wget;wait
 #repo dummy email
-git config user.email "fufufafa@gmail.com"
-git config user.name "fufufafa"
+git config --global user.email "fufufafa@gmail.com"
+git config --global user.name "fufufafa"
