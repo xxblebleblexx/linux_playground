1b769db7110415188dc5e04cd65179077925f7cc
blebleble save ur day <108147789+xxblebleblexx@users.noreply.github.com>
2026-09-15 20:45:10 +0700

    Change kernel branch from 'dev' to 'rework'

--- kernel.sh
+++ kernel.sh
@@ -1,6 +1,6 @@
 #configuration
 kernel_source=https://github.com/xxblebleblexx/android_kernel_xiaomi_gale.git
-branch_kernel=dev
+branch_kernel=rework
 defconfig_path=arch/arm64/configs/moonbeam_defconfig
 defconfig=moonbeam_defconfig
 
