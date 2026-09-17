739d5a43b8a46888189a8321197ec145377e7426
blebleble save ur day <108147789+xxblebleblexx@users.noreply.github.com>
2026-09-17 20:31:30 +0700

    change sakura clang to latest neutron clang

--- toolchain.sh
+++ toolchain.sh
@@ -3,11 +3,11 @@
 #setup mandatory packages
 sudo pacman -Sy --noconfirm git patch llvm lld base-devel ncurses openssl bc flex bison rsync kmod cpio python ccache tar gzip zstd wget;wait
 #Toolchain compiler
-wget https://github.com/Madara273/Sakura-Clang-Compiler/releases/download/Sakura-Clang-R01/sakura-clang-24.0.0.tar.zst;wait
+wget https://github.com/Neutron-Toolchains/clang-build-catalogue/releases/download/06092026/neutron-clang-06092026.tar.zst;wait
 mkdir -p clang
-mv sakura-clang-24.0.0.tar.zst clang/
+mv neutron-clang-06092026.tar.zst clang/
 cd clang
-tar --zstd -xf sakura-clang-24.0.0.tar.zst
+tar --zstd -xf neutron-clang-06092026.tar.zst
 
 #MLGO
 wget https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/refs/heads/main/mlgo-models.tar.gz
