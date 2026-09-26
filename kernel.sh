c5cb04acede1193a273402af7c0be734ffa42c53
blebleble save ur day <108147789+xxblebleblexx@users.noreply.github.com>
2026-09-26 10:12:22 +0700

    Change kernel branch from 'pre' to 'sync'

--- kernel.sh
+++ kernel.sh
@@ -1,6 +1,6 @@
 #configuration
 kernel_source=https://github.com/xxblebleblexx/android_kernel_xiaomi_gale.git
-branch_kernel=pre
+branch_kernel=sync
 defconfig_path=arch/arm64/configs/moonbeam_defconfig
 defconfig=moonbeam_defconfig
 
